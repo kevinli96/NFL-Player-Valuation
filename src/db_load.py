fc6c8bfae719f2f596441300bf0590566ca528b8
Edan Eingal <edan_eingal@brown.edu>
2017-03-12 18:50:48 -0400

    working on loading to db

--- src/db_load.py
+++ src/db_load.py
@@ -4,11 +4,6 @@ import csv
 
 def load():
 
-    ###################################################
-    # TODO:                                           #
-    # Fill in this function so that it loads your     #
-    # data into a file called playlist_data.db.       #
-    ###################################################
     conn = sqlite3.connect('nfl_valuation_data.db')
     c = conn.cursor()
 
@@ -18,7 +13,6 @@ def load():
     c.execute('DROP TABLE IF EXISTS "av";')
     c.execute('DROP TABLE IF EXISTS "team";')
     c.execute('DROP TABLE IF EXISTS "position";')
-    c.execute('DROP TABLE IF EXISTS "plays";')
     c.execute('DROP TABLE IF EXISTS "draft";')
 
     # Create tables
@@ -45,9 +39,14 @@ def load():
             CREATE TABLE av(
                 player_id int not null,
                 year int,
-                av_value int,
                 team_id text,
                 position_id int,
+                age int,
+                av_value int,
+                games_played int,
+                games_Started int,
+                pro_bowler int,
+                all_pro int,
                 FOREIGN KEY (player_id) REFERENCES player(id),
                 FOREIGN KEY (team_id) REFERENCES team(id),
                 FOREIGN KEY (position_id) REFERENCES position(id))
@@ -68,17 +67,6 @@ def load():
                 ''')
 
     c.execute('''
-            CREATE TABLE plays(
-                player_id int not null,
-                year int,
-                team_id text,
-                position_id int,
-                FOREIGN KEY (player_id) REFERENCES player(id),
-                FOREIGN KEY (team_id) REFERENCES team(id),
-                FOREIGN KEY (position_id) REFERENCES position(id))
-                ''')
-
-    c.execute('''
             CREATE TABLE draft(
                 player_id int not null,
                 year int,
@@ -94,8 +82,11 @@ def load():
     conn.commit()
 
     player_data = {}
+    undrafted_player_data = {}
+    undrafted_year = '0000'
+    undrafted_pick = 1
 
-    with open('team_data.csv', 'r', encoding='utf-8') as csvfile:
+    with open('../data/team_data.csv', 'r', encoding='utf-8') as csvfile:
         csv_reader = csv.reader(csvfile)
         next(csv_reader, None)
         for row in csv_reader:
@@ -107,7 +98,7 @@ def load():
                 VALUES (?, ?)''',
                 (team_id,team_name))
 
-    with open('position_data.csv', 'r', encoding='utf-8') as csvfile:
+    with open('../data/position_data.csv', 'r', encoding='utf-8') as csvfile:
         csv_reader = csv.reader(csvfile)
         next(csv_reader, None)
         for row in csv_reader:
@@ -119,7 +110,7 @@ def load():
                 VALUES (?, ?)''',
                 (position_id,position_name))
 
-    with open('draft_data.csv', 'r', encoding='utf-8') as csvfile:
+    with open('../data/draft_data.csv', 'r', encoding='utf-8') as csvfile:
         csv_reader = csv.reader(csvfile)
         next(csv_reader, None)
         for row in csv_reader:
@@ -132,7 +123,7 @@ def load():
 
             player_id = str(year) + str(pick)
 
-            player_data[player_id] = player_name
+            player_data[(player_name,draft_round,pick)] = player_id
 
             c.execute('''
                 INSERT INTO player
@@ -144,24 +135,56 @@ def load():
                 VALUES (?, ?, ?, ?, ?)''',
                 (player_id, year, team_id, draft_round, pick, position_id))
 
-    with open('av_data.csv', 'r', encoding='utf-8') as csvfile:
+    with open('../data/av_data.csv', 'r', encoding='utf-8') as csvfile:
         csv_reader = csv.reader(csvfile)
         next(csv_reader, None)
         for row in csv_reader:
-            player_id = row[0]
-            team_name = row[1]
+            player_name = row[1]
+            player_url = row[2]
+            year = row[3]
+            age = row[4]
+            draft = row[5]
+            team_id = row[6]
+            games_played = row[8]
+            games_started = row[9]
+            pro_bowler = row[11]
+            all_pro = row[12]
+            av_value = row[13]
+            position_id = row[14]
+
+            draft_split = draft.split('-')
+            player_id = player_data[(player_name,draft_split[0],draft_split[1])]
+
+            if player_id is None:
+                if undrafted_player_data[player_url] is None:
+                    player_id = undrafted_year + str(undrafted_pick)
+                    undrafted_pick = undrafted_pick + 1
+                    undrafted_player_data[player_url] = player_id
+                    c.execute('''
+                            INSERT INTO player
+                            VALUES (?, ?)''',
+                            (player_id,player_name))
+                else:
+                    player_id = undrafted_player_data[player_url]
 
             c.execute('''
                 INSERT INTO av
-                VALUES (?, ?)''',
-                (team_id,team_name))
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+                (player_id, year, team_id, position_id, age, av_value, games_played, games_started, pro_bowler, all_pro))       
+
+    # with open('../data/salary_data.csv', 'r', encoding='utf-8') as csvfile:
+    #     csv_reader = csv.reader(csvfile)
+    #     next(csv_reader, None)
+    #     for row in csv_reader:
+    #         NEED TO DO THIS
+
+    #         c.execute('''
+    #             INSERT INTO salary
+    #             VALUES (?, ?, ?, ?, ?)''',
+    #             (player_id, year, team_id, position_id, salary))     
 
     conn.commit()
     conn.close()
-    ###################################################
-    #               END OF YOUR CODE                  #
-    ###################################################
-
 
 if __name__ == '__main__':
     load()
