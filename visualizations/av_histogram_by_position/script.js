bd76cf59b469a2673213c175a5cd0a2533459fab
kevinli96 <kerui_li@brown.edu>
2017-03-14 18:12:38 -0400

    Added chart to blog

--- visualizations/av_histogram_by_position/script.js
+++ visualizations/av_histogram_by_position/script.js
@@ -6,33 +6,11 @@
  var pic_height = 400, // height of svg image
      pic_width = 700, // width of svg image
      background_color = tan, // background color of image
-     color_range = [yellow, red, cyan], // transitions of color
+     color_range = [yellow, cyan, red], // transitions of color
      bar_spacing = 0.2 // spacing between each bar
 
  var MIN_AV = -5
  var MAX_AV = 26
-
- var bardata = [] // empty array
-
- // pushing random data values onto bardata
-
- // MODIFY BELOW
- // such that bardata is filled with 32 values, each denoting
- // the counts corresponding to the AV at that index, with the domain
- // of the AVs going from -5 to 26
- // i.e. [15, 25, 30, ..., 20, 14]
- // there are 15 people with an AV of -5, 25 with an AV of -4
- // 20 with an AV of 25, 14 with an AV of 14
-
- for (var i = -5; i <= 26; i++) {
-     bardata.push(Math.random() * 100);
- }
- console.log(bardata);
-
- // MODIFY ABOVE
-
- // Tooltip for hovering over bars to display value
- var tooltip = d3.select('body').append('div').attr("class", "tooltip")
 
  // setting margins for the svg
  var margin = {
@@ -48,75 +26,101 @@
  var height = pic_height - margin.top - margin.bottom,
      width = pic_width - margin.left - margin.right;
 
- // linear color scale - feel free to play around with the color_range 
- // array defined above
- var colors = d3.scaleLinear()
-     .domain([0, bardata.length * .5, bardata.length])
-     .range(color_range)
-
- // yscale goes from 0 to the maximum count of AV in bardata, ranging from
- // 0 to the chart height
- var yScale = d3.scaleLinear().domain([0, d3.max(bardata)]).range([0, height]);
-
- // xscale has a domain of the minimum AV value (-5) and the maximum AV value (26)
- var xScale = d3.scaleBand()
-     .domain(d3.range(MIN_AV, MAX_AV + 1))
-     .range([0, width])
-     .paddingInner(bar_spacing)
-
- var svg = d3.select('#chart').append('svg')
-     .style('background', background_color)
-     .attr('width', width + margin.left + margin.right)
-     .attr('height', height + margin.top + margin.bottom)
-     .append('g')
-     .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
-     .selectAll('rect').data(bardata)
-     .enter().append('rect')
-     .style('fill', function(d, i) {
-         return colors(i);
+ function drawChart(chartId, position) {
+     var bardata = [];
+     d3.tsv(position + '_AV_counts.tsv', function(data) {
+         for (key in data) {
+             bardata.push(parseInt(data[key].value))
+         }
+         drawHistogram(chartId, bardata, position);
      })
-     .attr('width', xScale.bandwidth())
-     .attr('x', function(d, i) {
-         return xScale(i - 5);
-     })
-     .attr('height', function(d) {
-         return yScale(d);
-     })
-     .attr('y', function(d) {
-         return height - yScale(d);
-     })
-
- // ABOVE: height and y are sort of counterintuitive, because of SVG graphics
- // centering everything around a (0,0) coordinate at the top left of the SVG.
-
- var xAxis = d3.axisBottom(xScale).tickValues(d3.range(MIN_AV, MAX_AV + 1))
-
- var horizontal = d3.select('svg').append('g')
-     .attr("class", "x axis")
-     .attr('transform', 'translate(' + margin.left + ', ' + (height + margin.top) + ')')
-     .call(xAxis)
-
-// Applying mouseover functions to rectangles
- d3.selectAll("rect")
-     .on("mouseover", mouseover)
-     .on("mousemove", function(d, i) {
-         mousemove(d)
-     })
-     .on("mouseout", mouseout);
-
- // TOOLTIP FUNCTIONS
-
- function mouseover() {
-     tooltip.transition().style("opacity", 0.9);
  }
 
- function mousemove(data) {
-     console.log(data);
-     tooltip.text(data)
-         .style("left", (d3.event.pageX - 34) + "px")
-         .style("top", (d3.event.pageY - 12) + "px");
- }
 
- function mouseout() {
-     tooltip.style("opacity", 0);
+ function drawHistogram(chartId, bardata, position) {
+     bardata = bardata.slice(0, 32)
+
+     var tooltip = d3.select('body').append('div').attr("class", "tooltip")
+         // linear color scale - feel free to play around with the color_range 
+         // array defined above
+     var colors = d3.scaleLinear()
+         .domain([0, bardata.length * .5, bardata.length])
+         .range(color_range)
+
+     // yscale goes from 0 to the maximum count of AV in bardata, ranging from
+     // 0 to the chart height
+     var yScale = d3.scaleLinear().domain([0, d3.max(bardata)]).range([0, height]);
+
+     // xscale has a domain of the minimum AV value (-5) and the maximum AV value (26)
+     var xScale = d3.scaleBand()
+         .domain(d3.range(MIN_AV, MAX_AV + 1))
+         .range([0, width])
+         .paddingInner(bar_spacing)
+     var svg = d3.select('#' + chartId).append('svg')
+         .style('background', background_color)
+         .attr('width', width + margin.left + margin.right)
+         .attr('height', height + margin.top + margin.bottom)
+         .append('g')
+         .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+         .selectAll('rect').data(bardata)
+         .enter().append('rect')
+         .style('fill', function(d, i) {
+             return colors(i);
+         })
+         .attr('width', xScale.bandwidth())
+         .attr('x', function(d, i) {
+             return xScale(i - 5);
+         })
+         .attr('height', function(d) {
+             return yScale(d);
+         })
+         .attr('y', function(d) {
+             return height - yScale(d);
+         })
+
+
+     d3.select('svg').append("text")
+         .attr("x", (width / 2))
+         .attr("y", 50)
+         .attr("text-anchor", "middle")
+         .style("font-size", "24px")
+         .text(position + " AV Counts");
+
+     d3.select('svg').append("g")
+         .attr("class", "legendLinear")
+         .attr("transform", "translate(20,20)");
+
+     var legendLinear = d3.legendColor()
+         .shapeWidth(30)
+         .orient('horizontal')
+         .scale(colors);
+
+     svg.select(".legendLinear")
+         .call(legendLinear);
+
+     // ABOVE: height and y are sort of counterintuitive, because of SVG graphics
+     // centering everything around a (0,0) coordinate at the top left of the SVG.
+
+     var xAxis = d3.axisBottom(xScale).tickValues(d3.range(MIN_AV, MAX_AV + 1))
+
+     var horizontal = d3.select('svg').append('g')
+         .attr("class", "x axis")
+         .attr('transform', 'translate(' + margin.left + ', ' + (height + margin.top) + ')')
+         .call(xAxis)
+
+     // Applying mouseover functions to rectangles
+     d3.selectAll("rect")
+         .on("mouseover", function(d) {
+             tooltip.transition().style("opacity", 0.9);
+         })
+         .on("mousemove", function(d, i) {
+             tooltip.text(d)
+                 .style("left", (d3.event.pageX - 34) + "px")
+                 .style("top", (d3.event.pageY - 12) + "px");
+         })
+         .on("mouseout", function(d) {
+             tooltip.style("opacity", 0);
+         });
  }
+ 
+ drawChart("chart", "QB")
