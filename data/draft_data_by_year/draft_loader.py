a4c71b809e6ad6530d605f7b9a1880e38f888bfe
kevinli96 <kerui_li@brown.edu>
2017-04-04 17:36:33 -0400

    finished draft years of 1980 to 1993

---
+++ data/draft_data_by_year/draft_loader.py
@@ -0,0 +1,15 @@
+import csv
+
+def main():
+    with open('draft_1980_1993.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["Year","Rnd","Pick","Tm","Player","Pos","Age","To","St","CarAV","DrAV","G"])
+        for year in range(1980, 1994):
+            with open('draft_' + str(year) + '.csv', 'r') as g:
+                reader = csv.reader(g)
+                for line in reader:
+                    line = [x.replace(' HOF', '') for x in line]
+                    writer.writerow([year] + line)
+
+if __name__ == '__main__':
+    main()
