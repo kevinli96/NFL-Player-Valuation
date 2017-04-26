27423976f994906fb9e78cd92d70d529bc28850f
kevinli96 <kerui_li@brown.edu>
2017-04-25 23:00:21 -0400

    changes to draft viz

--- blog/viz/draft_av_by_position/script.py
+++ blog/viz/draft_av_by_position/script.py
@@ -1,10 +1,13 @@
 import csv
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
 
 def get_average_AV():
     AV_by_position = {}
-    roundCounts = []
+    roundCounts = {}
     for i in range(0, 12):
-        roundCounts.append(0)
+        roundCounts[i + 1] = {}
     with open('draft_data.csv', 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         next(reader, None)
@@ -14,18 +17,19 @@ def get_average_AV():
             rd = int(line[1])
             if line[9] == '':
                 continue
-            AV = float(line[9])
+            endYear = int(line[7]) if line[7] != '' else 2017
+            AV = round(float(line[9])/(endYear + 1 - int(line[0])), 3)
             AV_by_position[position][rd] = AV + AV_by_position[position].get(rd, 0)
-            roundCounts[rd - 1] += 1
+            roundCounts[rd][position] = 1 + roundCounts[rd].get(position, 0)
+    pp.pprint(AV_by_position)
     for pos in AV_by_position:
         counts = AV_by_position[pos]
         for i in range(0, 12):
             if i + 1 in counts:
-                counts[i + 1] /= roundCounts[i]
+                counts[i + 1] /= roundCounts[i + 1][pos]
                 counts[i + 1] = round(counts[i + 1], 3)
-
-    # print(roundCounts)
-    # print(AV_by_position)
+    pp.pprint(roundCounts)
+    pp.pprint(AV_by_position)
 
     return AV_by_position
 
@@ -46,8 +50,9 @@ def write_to_CSV(AV_by_position):
             if g1 != 0 and g2 != 0 and g3 != 0 and g4 != 0:
                 toWrite.append([pos, g1, g2, g3, g4])
                 # writer.writerow([pos, g1, g2, g3, g4])
-        print(toWrite)
+
         toWrite.sort(key=lambda item: item[1] + item[2] + item[3] + item[4])
+        print(toWrite)
         for row in toWrite:
             writer.writerow(row)
 
